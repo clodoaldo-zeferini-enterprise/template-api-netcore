44ad3e5319c9b06f05ce66b1f8c9212588250731
= <clodoaldo.zeferini.enterprise@outlook.com>
2023-01-28 14:46:52 -0300

    Removendo Code Smell

--- src/Service.Template.Repository/Interfaces/Repositories/Base/IRepositoryBase.cs
+++ src/Service.Template.Repository/Interfaces/Repositories/Base/IRepositoryBase.cs
@@ -16,40 +16,6 @@ namespace Service.Template.Domain.Interfaces.Repositories.Base
             Func<GridReader, IEnumerable<T2>> func2,
             Func<GridReader, IEnumerable<T3>> func3);
 
-        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>> GetMultiple<T1, T2, T3, T4>(DapperQuery dapperQuery, object parameters,
-            Func<GridReader, IEnumerable<T1>> func1,
-            Func<GridReader, IEnumerable<T2>> func2,
-            Func<GridReader, IEnumerable<T3>> func3,
-            Func<GridReader, IEnumerable<T4>> func4
-        );
-
-        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>> GetMultiple<T1, T2, T3, T4, T5>(DapperQuery dapperQuery, object parameters,
-            Func<GridReader, IEnumerable<T1>> func1,
-            Func<GridReader, IEnumerable<T2>> func2,
-            Func<GridReader, IEnumerable<T3>> func3,
-            Func<GridReader, IEnumerable<T4>> func4,
-            Func<GridReader, IEnumerable<T5>> func5
-        );
-
-        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>> GetMultiple<T1, T2, T3, T4, T5, T6>(DapperQuery dapperQuery, object parameters,
-            Func<GridReader, IEnumerable<T1>> func1,
-            Func<GridReader, IEnumerable<T2>> func2,
-            Func<GridReader, IEnumerable<T3>> func3,
-            Func<GridReader, IEnumerable<T4>> func4,
-            Func<GridReader, IEnumerable<T5>> func5,
-            Func<GridReader, IEnumerable<T6>> func6
-        );
-
-        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>, IEnumerable<T4>, IEnumerable<T5>, IEnumerable<T6>, IEnumerable<T7>> GetMultiple<T1, T2, T3, T4, T5, T6, T7>(DapperQuery dapperQuery, object parameters,
-            Func<GridReader, IEnumerable<T1>> func1,
-            Func<GridReader, IEnumerable<T2>> func2,
-            Func<GridReader, IEnumerable<T3>> func3,
-            Func<GridReader, IEnumerable<T4>> func4,
-            Func<GridReader, IEnumerable<T5>> func5,
-            Func<GridReader, IEnumerable<T6>> func6,
-            Func<GridReader, IEnumerable<T7>> func7
-        );
-
         Task<IEnumerable<TEntity>> Get(DapperQuery dapperQuery);
         Task<IEnumerable<TEntity>> GetAll();
         Task<TEntity> GetById(Guid id);
