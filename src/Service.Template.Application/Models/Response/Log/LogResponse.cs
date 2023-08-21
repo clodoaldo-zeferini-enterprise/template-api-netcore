b9b6df2868d8b96b95722cfdae5d448d4dc243e3
ClodoaldoZeferiniUnivesp <2109665@aluno.univesp.br>
2023-08-21 19:59:10 -0300

    Testado

---
+++ src/Service.Template.Application/Models/Response/Log/LogResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Template.Application.Models.STS
+{
+    public class LogResponse
+    {
+        public bool IsValid { get; set; }
+
+        private LogResponse() { }
+
+        public LogResponse(bool isValid) {
+            IsValid = IsValid;
+
+        }
+    }
+}
